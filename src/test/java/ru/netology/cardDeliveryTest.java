e9f5270c8d91255be68c732e063ebcb3e4932f5b
zenitfan88 <nikanenkov@rambler.ru>
2022-11-02 07:28:46 +0400

    fixed data

--- src/test/java/ru/netology/cardDeliveryTest.java
+++ src/test/java/ru/netology/cardDeliveryTest.java
@@ -4,7 +4,12 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -21,10 +26,14 @@ public class cardDeliveryTest {
 
     @Test
     void cardDeliveryTestNew() {
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(c.getTime());
         $x("//input[@placeholder='Город']").val("Ульяновск");
         $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.CONTROL+"A");
         $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder='Дата встречи']").val("06.11.2022");
+        $x("//input[@placeholder='Дата встречи']").val(str);
         $x("//input[@name='name']").val("Иван Иванов");
         $x("//input[@name='phone']").val("+79278529614");
         $x("//label[@data-test-id='agreement']").click();
