946b1a120e2affea415775006868b64de80cc6eb
zenitfan88 <nikanenkov@rambler.ru>
2022-11-06 16:11:32 +0400

    fixed test

---
+++ src/test/java/ru/netology/СardDeliveryTestNotPass.java
@@ -0,0 +1,175 @@
+package ru.netology;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+
+public class СardDeliveryTestNotPass {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 10;
+    }
+
+    private void assertTrue(String element) {
+    }
+
+    @Test
+    void cardDeliveryTestNotPassCity() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("New-York");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Иван Иванов");
+        $x("//input[@name='phone']").val("+79278529614");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='city']//span[@class='input__sub']").shouldHave(exactText("Доставка " +
+                "в выбранный город недоступна"));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassNoCity() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Иван Иванов");
+        $x("//input[@name='phone']").val("+79278529614");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='city']//span[@class='input__sub']").shouldHave(exactText("Поле " +
+                "обязательно для заполнения"));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassData() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 2);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("Ульяновск");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Иван Иванов");
+        $x("//input[@name='phone']").val("+79278529614");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='date']//span[@class='input__sub']").shouldHave(exactText("Заказ " +
+                "на выбранную дату невозможен"));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassNoData() {
+        $("span[data-test-id='city'] input").val("Ульяновск");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Иван Иванов");
+        $x("//input[@name='phone']").val("+79278529614");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='date']//span[@class='input__sub']").shouldHave(exactText("Неверно " +
+                "введена дата"));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassName() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("Ульяновск");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Ivan");
+        $x("//input[@name='phone']").val("+79278529614");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='name']//span[@class='input__sub']").shouldHave(exactText("Имя и " +
+                "Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassNoName() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("Ульяновск");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("");
+        $x("//input[@name='phone']").val("+79278529614");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='name']//span[@class='input__sub']").shouldHave(exactText("Поле " +
+                "обязательно для заполнения"));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassPhone() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("Ульяновск");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Иван Иванов");
+        $x("//input[@name='phone']").val("+7927852961");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='phone']//span[@class='input__sub']").shouldHave(exactText("Телефон указан " +
+                "неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void cardDeliveryTestNotPassNoCheckBox() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(calendar.getTime());
+        $("span[data-test-id='city'] input").val("Ульяновск");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.CONTROL
+                + "A");
+        $x("//*[@data-test-id='date']//input[@class='input__control']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@data-test-id='date']//input[@class='input__control']").val(str);
+        $x("//*[@data-test-id='name']//input[@class='input__control']").val("Иван Иванов");
+        $x("//input[@name='phone']").val("+79279631423");
+        $x("//*[text()='Забронировать']").click();
+
+        $("label[data-test-id='agreement']").shouldHave(cssClass("input_invalid"));
+    }
+
+}
